3a94f412a548c5d5b7edfb7d65fe2933643b3524
MrMahi-07 <mrmahi353@gmail.com>
2025-03-20 20:13:00 +0530

    seeding feature added to stream user function

--- dags/kafka_streaming.py
+++ dags/kafka_streaming.py
@@ -1,13 +1,16 @@
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.python import PythonOperator
 from datetime import datetime
 from faker import Faker
 from json import dumps
 from kafka import KafkaProducer
 from random import uniform
-from time import sleep
+from time import sleep, time
 from typing import Any
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 default_args = {
@@ -18,8 +21,8 @@ fake = Faker()
 
 
 def generate_user():
-    sleep(uniform(0.0, 1.5))
-    
+    sleep(uniform(2, 5))
+
     return {
         "name": fake.name(),
         "email": fake.email(),
@@ -32,15 +35,21 @@ def generate_user():
     }
 
 
-def stream_user(user: dict[str, Any]):
+def stream_user():
     producer = KafkaProducer(
         bootstrap_servers="broker:29092",  # <-- Very important!
         value_serializer=lambda v: dumps(v).encode("utf-8"),
     )
-    print(f"User: {user['name']}, Email: {user['email']}")
-    producer.send("user", value=user)
-    producer.flush()
-    producer.close()
+
+    start_time = time()
+    while (time() - start_time) < 560:
+
+        user = generate_user()
+
+        logger.info(f"User: {user['name']}, Email: {user['email']}")
+        producer.send("user", value=user)
+    # producer.flush()
+    # producer.close()
 
 
 with DAG(
@@ -52,6 +61,8 @@ with DAG(
     tags=["kafka", "streaming"],
 ) as dag:
 
-    streaming_task = PythonOperator(
-        task_id="stream_user_from_faker", python_callable=stream_user
-    )
+    @task()
+    def stream_user_task(user: dict[str, Any]):
+        stream_user(user)
+
+    stream_user_task()
